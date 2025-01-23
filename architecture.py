9233a5bdea5bbacc7af0888f209026905fac6ab5
ameerothman <arothman@wustl.edu>
2025-01-23 01:17:55 -0500

    Assignment 4

---
+++ architecture.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+#from config import *
+
+class vgg_face(nn.Module):
+    """
+    Main Class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        self.block_size = [2, 2, 3, 3, 3]
+        self.conv_1_1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
+        self.conv_1_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.conv_2_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.conv_2_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.conv_3_1 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.conv_3_2 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv_3_3 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv_4_1 = nn.Conv2d(256, 512, 3, stride=1, padding=1)
+        self.conv_4_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_4_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_1 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.fc6 = nn.Linear(512 * 7 * 7, 1024)
+        self.fc7 = nn.Linear(1024, 1024)
+        self.fc8 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        """ Pytorch forward
+
+        Args:
+            x: input image (224x224)
+
+        Returns: class logits
+
+        """
+        x = F.relu(self.conv_1_1(x))
+        x = F.relu(self.conv_1_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_2_1(x))
+        x = F.relu(self.conv_2_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_3_1(x))
+        x = F.relu(self.conv_3_2(x))
+        x = F.relu(self.conv_3_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_4_1(x))
+        x = F.relu(self.conv_4_2(x))
+        x = F.relu(self.conv_4_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_5_1(x))
+        x = F.relu(self.conv_5_2(x))
+        x = F.relu(self.conv_5_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc6(x))
+        x = F.dropout(x, 0.5, self.training)
+        x = F.relu(self.fc7(x))
+        x = F.dropout(x, 0.5, self.training)
+        return self.fc8(x)
+
+class normalizeLayer(torch.nn.Module):
+    """Standardize the channels of a batch of images by subtracting the dataset mean
+      and dividing by the dataset standard deviation.
+      In order to certify radii in original coordinates rather than standardized coordinates, we
+      add the Gaussian noise _before_ standardizing, which is why we have standardization be the first
+      layer of the classifier rather than as a part of preprocessing as is typical.
+      """
+
+    def __init__(self, means, sds):
+        """
+        :param means: the channel means
+        :param sds: the channel standard deviations
+        """
+        super(normalizeLayer, self).__init__()
+        self.means = torch.tensor(means)
+        self.sds = torch.tensor(sds)
+
+    def forward(self, input: torch.tensor):
+        (batch_size, num_channels, height, width) = input.shape
+        means = self.means.repeat((batch_size, height, width, 1)).permute(0, 3, 1, 2)
+        sds = self.sds.repeat((batch_size, height, width, 1)).permute(0, 3, 1, 2)
+        return (input - means) / sds
+    
+def get_normalize_layer(means, stds):
+    """Return the dataset's normalization layer"""
+    return normalizeLayer(means, stds)
+
+def restore_model():
+    """
+    Restore the old model with inserting a normalize layer in front of the CNNs.
+    """
+
+    dataset_mean, dataset_std = [0.367035294117647,0.41083294117647057,0.5066129411764705], [1/255., 1/255., 1/255.]
+    normalize_layer = get_normalize_layer(dataset_mean, dataset_std)
+
+    model = vgg_face()
+    model.load_state_dict(torch.load('new_ori_model.pt', map_location='cpu'))
+    model = torch.nn.Sequential(normalize_layer, model)
+    torch.save(model.state_dict(), 'vgg_face.pt') 
+
+def get_model():
+    """
+    Return the architecture given a dataset. Note that a nomalize layer is inserted.
+    """
+
+    dataset_mean, dataset_std = [0.367035294117647,0.41083294117647057,0.5066129411764705], [1/255., 1/255., 1/255.]
+    normalize_layer = get_normalize_layer(dataset_mean, dataset_std)
+    model = vgg_face()
+    return torch.nn.Sequential(normalize_layer, model) 
